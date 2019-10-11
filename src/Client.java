cc819a877f3b3fb9b25660dc8c180145c5fa5ea4
Sam Fields <fields8@uw.edu>
2019-10-11 12:47:04 -0700

    added comments, changed print structure of printAll()

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,7 @@
 
 public class Client {
 	public static void main(String[] args) {
-		SvobodaDictionary s1 = new SvobodaDictionary("test1");
+		SvobodaDictionary s1 = new SvobodaDictionary("test1.txt");
 		s1.printAll();
 	}
 }
--- src/SvobodaDictionary.java
+++ src/SvobodaDictionary.java
@@ -1,15 +1,19 @@
 import java.util.*;
 import java.io.*;
 
+//Dictionary object. Reads in a .txt file from the txt folder specified by a passed file name and contructs a map of the unique words
+//	with the key being the first letter of the word
 public class SvobodaDictionary {
 	private Map<Character, TreeSet<String>> SvobodaDictionary;
 	
+	//Constructor
+	//fileName: Name of the .txt file (include .txt tag) stored in the txt folder
 	public SvobodaDictionary(String fileName) {
 		SvobodaDictionary = new TreeMap<Character, TreeSet<String>>();
 		
 		Scanner input = null;
 		try {
-			input = new Scanner(new File("../txt/" + fileName + ".txt"));
+			input = new Scanner(new File("../txt/" + fileName));
 		} catch (FileNotFoundException e) {
 			System.exit(1);
 		}
@@ -29,11 +33,14 @@ public class SvobodaDictionary {
 		input.close();
 	}
 	
+	//Prints to terminal all the words in the dictionary in alphabetical order
 	public void printAll() {
 		for (char letter: SvobodaDictionary.keySet()) {
+			System.out.println(letter +  ":");
 			for (String word: SvobodaDictionary.get(letter)) {
 				System.out.println(word);
 			}
+			System.out.println();
 		}
 	}
 }
