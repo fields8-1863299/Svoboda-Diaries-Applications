6a43eb82d44a235ab0bcad05b15a4aa94b350c28
Sam Fields <fields8@uw.edu>
2019-11-15 13:52:04 -0800

    added terminal ui to client

--- src/Client.java
+++ src/Client.java
@@ -3,13 +3,62 @@ import java.util.*;
 
 public class Client {
 	public static void main(String[] args) throws IOException {
-		SvobodaDictionary s1 = new SvobodaDictionary("diary.txt");
-		Queue<String> wordsTE = s1.findByLetters("the");
-		System.out.println(wordsTE);
-		wordsTE = s1.findByLetters("the", 4, 5);
-		System.out.println(wordsTE);
+		Scanner console = new Scanner(System.in);
+		
+		System.out.print("Enter txt file your want to read in: ");
+		String txtfile = console.next();
+		System.out.println();
+		
+		SvobodaDictionary dict = new SvobodaDictionary(txtfile);
+		
+		boolean esc = true;
+		
+		while (esc) {
+			System.out.println("Do you want to:");
+			System.out.println("   Export new CSV? (1)");
+			System.out.println("   Find words by letters? (2)");
+			System.out.println("   Find words by letters and length? (3)");
+			System.out.println("   Find words by letters and range? (4)");
+			int choice = console.nextInt();
+			System.out.println();
+			
+			if (choice == 1) {
+				System.out.print("Enter name for csv file: ");
+				String name = console.next();
+				System.out.println();
+				
+				dict.exportCSV(name, "v");
+				dict.exportCSV(name, "r");
+			} else if (choice == 2) {
+				System.out.print("Enter letters you're looking for in the sequence they appear: ");
+				String pattern = console.next();
+				System.out.println();
+				
+				System.out.println(dict.findByLetters(pattern));
+				
+			} else if (choice == 3) {
+				System.out.print("Enter letters you're looking for in the sequence they appear: ");
+				String pattern = console.next();
+				System.out.print("Enter a length: ");
+				int length = console.nextInt();
+				
+				System.out.println(dict.findByLetters(pattern, length));
+			} else if (choice == 4) {
+				System.out.print("Enter letters you're looking for in the sequence they appear: ");
+				String pattern = console.next();
+				System.out.print("Smallest word length: ");
+				int min = console.nextInt();
+				System.out.print("Largest word length: ");
+				int max = console.nextInt();
+				
+				System.out.println(dict.findByLetters(pattern, min, max));
+			}
+			
+			System.out.print("Enter \"Yes\" to Exit: ");
+			if (console.next().equals("Yes")) {
+				esc = false;
+			}
+			System.out.println();
+		}
 	}
-	
-	//updateDictionary(String fileName)
-	//Updates dictionary (dictionary folder) with words from passed txt folder
 }
